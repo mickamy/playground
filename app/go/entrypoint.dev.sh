b2432cd0b9792e2a2333040946e10fed5ad128f7
mickamy <tetsuro@mickamy.com>
2024-07-25 07:22:54 +0900

    chore: use mysqldef

--- app/go/entrypoint.dev.sh
+++ app/go/entrypoint.dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-psqldef -U $DB_USER -W $DB_PASS -h $DB_HOST -p $DB_PORT $DB_NAME < ./db/schema.sql
+mysqldef -U "$DB_USER" -W "$DB_PASS" -h "$DB_HOST" -p "$DB_PORT" "$DB_NAME" < ./db/schema.sql
 air
